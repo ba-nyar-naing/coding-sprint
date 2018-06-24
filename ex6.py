c038d4d9bbaca5958259154be3990d779de521d0
Ba Nyar Naing <banyarnaing123@gmail.com>
2018-06-24 16:49:43 +0630

    More Printing

--- ex6.py
+++ ex6.py
@@ -1,7 +1,7 @@
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
-y = "Those who know %s and those who %s." %(binary, do_not)
+y = "Those who know %s and those who %s." % (binary, do_not)
 
 print(x)
 print(y)
@@ -17,4 +17,4 @@ print(joke_evaluation % hilarious)
 w = "This is the left side of ..."
 e = "a string with a right side."
 
-print(w+e)
+print(w + e)
